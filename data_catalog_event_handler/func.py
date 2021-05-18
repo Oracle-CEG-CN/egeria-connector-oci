544c84800424fe8b870388a6be0f241401268998
Stefan Höning <Stefan.Hoening@oracle.com>
2021-05-18 16:23:06 +0200

    Added code to read oci client configuration from application environment.
    -   Environment values are added via web console to avoid
        putting authentication information in repository.

--- data_catalog_event_handler/func.py
+++ data_catalog_event_handler/func.py
@@ -1,23 +1,41 @@
 import io
 import json
 import logging
+import sys
 
 from fdk import response
 
+import oci
 
 def handler(ctx, data: io.BytesIO = None):
     name = "World"
-    if data != None:
-        logging.getLogger().info(data.getvalue())
-    try:
-        body = json.loads(data.getvalue())
-        name = body.get("name")
-    except (Exception, ValueError) as ex:
-        logging.getLogger().info('error parsing json payload: ' + str(ex))
 
-    logging.getLogger().info("Inside Python Hello World function")
+
+    oci_config = {}
+    env = ctx.Config()
+
+    for key in ['user', 'tenancy', 'region', 'key_content', 'fingerprint']:
+        env_key = 'oci_config_%s' % key
+        if env_key in env:
+            value = env[env_key]
+            if key == 'key_content':
+                import base64
+                value = base64.b64decode(value).decode('UTF-8')
+            oci_config[key] = value
+
+    from oci.config import validate_config
+    validate_config(oci_config)
+
+    catalog_client = oci.data_catalog.DataCatalogClient(oci_config)
+
+
+    response_json = {
+        "message": "Hello {0}".format(name),
+        "python_version": sys.version_info,
+        "oci_config": oci_config
+    }
+        
     return response.Response(
-        ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
+        ctx, response_data=json.dumps(response_json),
         headers={"Content-Type": "application/json"}
     )
