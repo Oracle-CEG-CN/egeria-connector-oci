f840d567fef5f9358e4e6486deca36e5910b4553
Stefan Höning <Stefan.Hoening@oracle.com>
2021-05-19 10:17:28 +0200

    Issue #1: Failing config check.
    Added code to handle Exceptions when calling validate_config.

--- data_catalog_event_handler/func.py
+++ data_catalog_event_handler/func.py
@@ -7,12 +7,11 @@ from fdk import response
 
 import oci
 
-def handler(ctx, data: io.BytesIO = None):
-    name = "World"
-
-
-    oci_config = {}
-    env = ctx.Config()
+def get_oci_config(env):
+    '''
+    Extract the oci configuration values from the given environment object.
+    '''
+    result = {}
 
     for key in ['user', 'tenancy', 'region', 'key_content', 'fingerprint']:
         env_key = 'oci_config_%s' % key
@@ -21,20 +20,42 @@ def handler(ctx, data: io.BytesIO = None):
             if key == 'key_content':
                 import base64
                 value = base64.b64decode(value).decode('UTF-8')
-            oci_config[key] = value
+            result[key] = value
 
-    from oci.config import validate_config
-    validate_config(oci_config)
+    return result
 
-    catalog_client = oci.data_catalog.DataCatalogClient(oci_config)
+def do_catalog_job(catalog_client):
+    '''
+    '''
+    pass
+
+def handler(ctx, data: io.BytesIO = None):
+    '''
+    Called as Oracle Function.
+
+    :param ctx the calling context, including environment and configuration of the function
+    :param data the data provided by the caller
+    '''
+    oci_config = get_oci_config(ctx.Config())
 
 
     response_json = {
-        "message": "Hello {0}".format(name),
         "python_version": sys.version_info,
-        "oci_config": oci_config
     }
         
+    from oci.config import validate_config
+    try:
+        validate_config(oci_config)
+        try:
+            do_catalog_job(oci.data_catalog.DataCatalogClient(oci_config))
+        except Exception as cje:
+            logging.error('Error during catalog job', cje)
+
+    except Exception as e:
+        response_json['error'] = str(e)
+        logging.error('Error during config validation', e)
+
+
     return response.Response(
         ctx, response_data=json.dumps(response_json),
         headers={"Content-Type": "application/json"}
