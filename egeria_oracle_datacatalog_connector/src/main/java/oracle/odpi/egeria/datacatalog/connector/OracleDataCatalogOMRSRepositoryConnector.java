194990325c5e3fc9e430c85fc22aa30d141d58fd
Stefan HC¶ning <Stefan.Hoening@oracle.com>
2021-05-27 14:45:28 +0000

    Started code to configure DataCatalogClient in connector.

--- egeria_oracle_datacatalog_connector/src/main/java/oracle/odpi/egeria/datacatalog/connector/OracleDataCatalogOMRSRepositoryConnector.java
+++ egeria_oracle_datacatalog_connector/src/main/java/oracle/odpi/egeria/datacatalog/connector/OracleDataCatalogOMRSRepositoryConnector.java
@@ -1,11 +1,42 @@
 package oracle.odpi.egeria.datacatalog.connector;
 
+import java.util.Map;
+
 import org.odpi.openmetadata.frameworks.connectors.properties.ConnectionProperties;
 
 import org.odpi.openmetadata.repositoryservices.connectors.stores.metadatacollectionstore.repositoryconnector.OMRSRepositoryConnector;
 
+import com.oracle.bmc.Region;
+
+import com.oracle.bmc.datacatalog.DataCatalogClient;
+
+import com.oracle.bmc.auth.SimpleAuthenticationDetailsProvider;
 
 public class OracleDataCatalogOMRSRepositoryConnector extends OMRSRepositoryConnector {
+
+    private static final String OCI_CONFIG_USER_ID = "oci-config-user-id";
+    private static final String OCI_CONFIG_TENANT_ID = "oci-config-tenant-id";
+    private static final String OCI_CONFIG_FINGERPRINT = "oci-config-fingerprint";
+    private static final String OCI_CONFIG_REGION = "oci-config-region";
+
+    private DataCatalogClient dataCatalogClient;
+
+
+    private static final DataCatalogClient createFromConnectionProperties(
+            final ConnectionProperties connectionProperties) {
+        Map<String, Object> connectionConfigurationProperties = connectionProperties.getConfigurationProperties();
+        SimpleAuthenticationDetailsProvider authenticationDetailsProvider;
+
+        authenticationDetailsProvider = SimpleAuthenticationDetailsProvider.builder()
+            .userId((String) connectionConfigurationProperties.get(OCI_CONFIG_USER_ID))
+            .tenantId((String) connectionConfigurationProperties.get(OCI_CONFIG_TENANT_ID))
+            .region(Region.fromRegionCodeOrId((String) connectionConfigurationProperties.get(OCI_CONFIG_REGION)))
+            .fingerprint((String) connectionConfigurationProperties.get(OCI_CONFIG_FINGERPRINT))
+            .build();
+
+        return null;
+    }
+
 
     @Override
     public void initialize(
